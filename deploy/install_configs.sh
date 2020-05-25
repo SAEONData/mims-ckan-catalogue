2835b301b12498d5d56ab858026aedcef4898afe
bryan@saeon.ac.za <bryan@saeon.ac.za>
2020-05-25 13:45:03 +0200

    adding deployment helper scripts

---
+++ deploy/install_configs.sh
@@ -0,0 +1,33 @@
+CKAN_INSTALL_DIR="/home/bmcalister/workspace/mims-catalogue/ckan/ckan/"
+
+echo "Copying configs in place"
+
+if [ -f "$CKAN_INSTALL_DIR/contrib/docker/solr/solrconfig.xml" ]; then
+   cp ./ckan-configs/solrconfig.xml $CKAN_INSTALL_DIR/contrib/docker/solr/
+else
+   echo  "$CKAN_INSTALL_DIR does not exist"
+fi
+
+if [ -f "$CKAN_INSTALL_DIR/ckan/config/solr/schema.xml" ]; then
+    cp ./ckan-configs/schema.xml $CKAN_INSTALL_DIR/ckan/config/solr/
+else
+   echo  "$CKAN_INSTALL_DIR does not exist"
+fi
+
+echo "Re-building SOLR docker image with new configurations"
+
+CURR_DIR=$(pwd)
+echo $CURR_DIR
+
+cd $CKAN_INSTALL_DIR/contrib/docker && docker stop solr && docker-compose build solr && docker-compose up -d solr
+
+cd $CURR_DIR
+
+sleep 5
+
+echo "Re-indexing SOLR for spatial extensions"
+
+docker exec -it ckan bash -c "source /usr/lib/ckan/venv/bin/activate && cd /usr/lib/ckan/venv/src/ && 
+	                      paster --plugin=ckan search-index rebuild --config=/etc/ckan/production.ini"
+
+
---
+++ deploy/install_plugins.sh
@@ -0,0 +1,17 @@
+
+docker exec -it ckan bash -c " source /usr/lib/ckan/venv/bin/activate && cd /usr/lib/ckan/venv/src/ && 
+	                       pip install -e git+https://github.com/ckan/ckanext-harvest.git@v1.2.0#egg=ckanext-harvest && 
+			       pip install -r /usr/lib/ckan/venv/src/ckanext-harvest/pip-requirements.txt && 
+			       paster --plugin=ckanext-harvest harvester initdb --config=/etc/ckan/production.ini"
+
+docker exec -it ckan bash -c " source /usr/lib/ckan/venv/bin/activate && 
+	                       cd /usr/lib/ckan/venv/src/ && 
+			       pip install -e git+https://github.com/ckan/ckanext-spatial.git#egg=ckanext-spatial &&
+			       git clone https://github.com/SAEONData/mims-ckan-catalogue.git &&
+			       cp -r mims-ckan-catalogue/ckanext-spatial/ckanext/spatial/* ckanext-spatial/ckanext/spatial/ 
+                               pip install -r /usr/lib/ckan/venv/src/ckanext-spatial/pip-requirements.txt"
+
+docker exec -it ckan bash -c " source /usr/lib/ckan/venv/bin/activate && 
+                               cd /usr/lib/ckan/venv/src/ &&
+			       cp -r mims-ckan-catalogue/ckanext-mims-catalogue/ ckanext-mims-theme/ &&
+			       cd ckanext-mims-theme/ && python setup.py develop"
